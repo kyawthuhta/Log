561adfb9eceb291aeb72f0c88e8c3e53e3404a95
Kyaw Thu Htay <kyawthu1085@gmail.com>
2019-07-12 16:37:33 +0630

    people

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_081825) do
+ActiveRecord::Schema.define(version: 2019_07_12_100550) do
 
   create_table "machines", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_07_12_081825) do
     t.string "address"
     t.string "phone"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "designation"
+    t.string "organization"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
